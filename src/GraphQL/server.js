27854d497e155f3bc0c2933e464aca1113bca174
문일윤 <mun-il-yun@mun-il-yun-ui-MacBookAir.local>
2023-12-27 12:31:06 +0900

    [GraphQL] Query Resolvers

--- src/GraphQL/server.js
+++ src/GraphQL/server.js
@@ -1,5 +1,17 @@
 import { ApolloServer, gql } from "apollo-server";
 
+// GraphQL에 알려줬던 형태를 따라야 한다.
+const tweets = [
+  {
+    id:"1",
+    text:"first one!",
+  },
+  {
+    id:"2",
+    text:"second one!",
+  }
+]
+
 // type Query는 restAPI에서 이 형태로 만든것과 같다
 // GET /text
 
@@ -7,24 +19,39 @@ import { ApolloServer, gql } from "apollo-server";
 // Scalar type, non-scalar type 혹은 root type은 graphql에 built-in 내장되어 있는 것 (String,Int,Boolean type존재)
 const typeDefs = gql`
   type User {
-    id: ID
-    username: String
+    id: ID!
+    username: String!
+    firstName: String!
+    lastName: String!
   }
   type Tweet {
-    id: ID
-    text:String
+    id: ID!
+    text: String!
     author: User
   }
   type Query {
-    allTweets: [Tweet]
-    tweet(id: ID): Tweet
+    allTweets: [Tweet!]!
+    tweet(id: ID!): Tweet
+    ping: String!
   }
   type Mutation {
-      postTweet(text:String, userId: ID): Tweet
+      postTweet(text:String!, userId: ID!): Tweet!
+      deleteTweet(id: ID!): Boolean!
   }
 `;
 
-const server = new ApolloServer({ typeDefs });
+const resolvers = {
+  Query: {
+    allTweets(){
+      return tweets;
+    },
+    tweet(root, {id}) {
+      return tweets.find((tweet) => tweet.id === id);
+    }
+  },
+}
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 // listen() 은 Promise이다.
 server.listen().then(({ url }) => {
